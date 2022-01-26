af04ec47f138b5d0c368392b4311aaa477bb3a80
ianlikescheese <98000008+ianlikescheese@users.noreply.github.com>
2022-01-25 18:52:02 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+
+function setup() {
+  createCanvas(400,400);
+  for(var i = 0; i < weight.length; i++){
+    console.log(weight[i])
+  }
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
